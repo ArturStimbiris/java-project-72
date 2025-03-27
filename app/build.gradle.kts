ef85cfc4fc9237d5c5d2c64cbc0b4cdf21c6ef3f
ArturStimbiris <artur.stimbirys@gmail.com>
2025-03-27 21:36:36 +0300

    make repo

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
